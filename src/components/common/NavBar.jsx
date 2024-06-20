5857adbc1d43533b55fdd42efd6e661ca9b622e2
unknown <maryamihsan601@gmail.com>
2024-06-20 18:45:34 +0300

    add class name for navbar

--- src/components/common/NavBar.jsx
+++ src/components/common/NavBar.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 import LoginBotton from '../bottons/LoginBotton'
 function NavBar() {
   return (
-    <div className='nav flex bg-[#323868] w-[90%] justify-center items-center rounded-[40px] mt-[40px]'>
+    <div className='nav  flex bg-[#323868] w-[90%] justify-center items-center rounded-[40px] mt-[40px]'>
       <ul className='items inline-flex m-3 gap-2 text-[15px]'>
       <LoginBotton/>
         <li className='item bg-[#181D3D] rounded-[25px] p-3 text-white'>الفرق الطبية</li>
