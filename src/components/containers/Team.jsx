8ab1d4bb810590fb00c4c89f73ebd69bbb5cf54d
nura <nooraliabdalhossen@gmail.com>
2024-06-20 14:43:06 +0300

    team is done without css

--- src/components/containers/Team.jsx
+++ src/components/containers/Team.jsx
@@ -1,43 +1,16 @@
 import React from "react";
-import NavBar from '../common/NavBar'
+import {TeamData} from  "../data/TeamData";
 
-export default function Team() {
+import TeamCard from "../cards/TeamCards"
+
+
+function Team() {
   return (
+    <div>
   
-    <div >
-        <NavBar/>
-     
-      <h1 className="text-center mt-10"> فرقنا </h1>
-      <div className="bg-slate-100 gap-5 grid grid-cols-1 mt-5 ">
-      
-        <div className="bg-slate-200 w-64 h-32 rounded-2xl  shadow-lg shadow-gray-500/30 mr-10 ">
-          <div className="bg-slate-100 rounded-2xl w-24 h-24 mt-2 mr-20 "></div>
-          <h3 className="mr-8 text-sm  mb-2">الاسم </h3>
-        </div>
-      </div>
-
-      <div className="bg-slate-100 gap-5 grid grid-cols-1 mt-5 ">
-        <div className="bg-slate-200 w-64 h-32 rounded-2xl  shadow-lg shadow-gray-500/30 mr-10 ">
-          <div className="bg-slate-100 rounded-2xl w-24 h-24 mt-2 mr-20 "></div>
-          <h3 className="mr-8 text-sm  mb-2">الاسم </h3>
-        </div>
-      </div>
-
-
-      <div className="bg-slate-100 gap-5 grid grid-cols-1 mt-5 ">
-        <div className="bg-slate-200 w-64 h-32 rounded-2xl  shadow-lg shadow-gray-500/30 mr-10 ">
-          <div className="bg-slate-100 rounded-2xl w-24 h-24 mt-2 mr-20 "></div>
-          <h3 className="mr-8 text-sm  mb-2">الاسم </h3>
-        </div>
-      </div>
-      <div className="bg-slate-100 gap-5 grid grid-cols-1 mt-5 ">
-        <div className="bg-slate-200 w-64 h-32 rounded-2xl  shadow-lg shadow-gray-500/30 mr-10 ">
-          <div className="bg-slate-100 rounded-2xl w-24 h-24 mt-2 mr-20 "></div>
-          <h3 className="mr-8 text-sm  mb-2">الاسم </h3>
-        </div>
-      </div>
-   
+    <TeamCard data={TeamData}/>
+  
     </div>
- 
   );
 }
+export default Team;
---
+++ src/components/data/TeamData.jsx
@@ -0,0 +1,12 @@
+import team from '../../assets/images/TeamWork.png'
+
+
+export const TeamData = [
+    { id: 1, title: 'تيم الاسكان  ', image: team},
+    { id: 2, title: 'تيم حي دراغ ',image: team },
+    { id: 3, title: 'تيم العامريه ',image: team },
+    { id: 4, title: 'تيم الجامعه ',image: team },
+    { id: 5, title: 'تيم الكاظميه',image: team },
+    { id: 6, title: 'تيم الاعظميه',image: team },
+ 
+  ];
