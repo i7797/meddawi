54dda671c206b5f15ab7d1a1b4a310d204d00303
nura <nooraliabdalhossen@gmail.com>
2024-06-20 17:31:56 +0300

    add service

--- src/AppRouter.jsx
+++ src/AppRouter.jsx
@@ -3,12 +3,14 @@ import { Route,  Routes } from 'react-router-dom'
 import Home from './pages/Home'
 import EmergencyContainer from './components/containers/EmergencyContainer'
 import Team from './components/containers/Team'
+import Services from './components/containers/Services'
 
 function AppRouter() {
   return (
     <div>
      <Routes>
         <Route path='/' element={<Home/>}/>
+        <Route path='/services' element={<Services/>}/>
         <Route  path='/emergency' element={<EmergencyContainer/>}/>
         <Route  path='/team' element={<Team/>}/>
      </Routes>
