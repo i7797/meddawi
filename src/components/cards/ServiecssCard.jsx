adceae358f2ce481c237df6b9f3d0ab437a2ce9b
nura <nooraliabdalhossen@gmail.com>
2024-06-20 19:03:07 +0300

    add Ask Service page

--- src/components/cards/ServiecssCard.jsx
+++ src/components/cards/ServiecssCard.jsx
@@ -4,6 +4,8 @@ import { Link } from "react-router-dom";
  function ServiecssCard({ data }) {
   return (
     <div>
+                  <Link  to="/AskService">  <button className='bg-red-400 rounded-lg w-80 h-10 text-xs text-center	mt-8 text-white	'> طلب خدمه الرعاية المستمرة  </button>  </Link>
+
        <div  className='   mt-28 '>    
             {data.map((item) => (
          <div className=' grid grid-cols-1 '>
@@ -11,7 +13,7 @@ import { Link } from "react-router-dom";
           <div key={item.id} className='bg-slate-100  w-40 h-40 mt-2 rounded-xl block ml-20' >
             <img className=' ml-1  w-36 h-32 mt-10  ' src={item.image} />
             <h2 className=' text-lg mt-10 text-center	' >{item.title}</h2>
-            <Link  to="/">  <button className='bg-red-300 rounded-sm w-20 mr-8 text-xs text-center	 ml-10'> احجز هنا  </button>  </Link>
+            <Link  to="/">  <button className='bg-blue-900 rounded-lg w-20 h-6 mr-8 text-xs text-center	 ml-10 text-white	'> احجز هنا  </button>  </Link>
 
           </div>
           </div>
