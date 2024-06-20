ec9a56911729839aa4cc938c66f44d6048968031
nura <nooraliabdalhossen@gmail.com>
2024-06-20 15:06:19 +0300

    add team css

--- src/components/cards/TeamCards.jsx
+++ src/components/cards/TeamCards.jsx
@@ -4,13 +4,13 @@ import { Link } from "react-router-dom";
  function TeamCard({ data }) {
   return (
     <div>
-        <div>
+        <div className='   mt-28 '>
         {data.map((item) => (
-         <div className='gap-5 grid grid-cols-2 mt-5 '>
-            <div className='bg-slate-200 w-40 h-48 rounded-2xl  shadow-lg shadow-gray-500/30  '>
-          <div key={item.id} className='bg-slate-100 rounded-2xl w-28 h-24 mt-2 mr-5 ' >
-            <img className=' h-20 w-20 mt-6 mb-20  mr-4' src={item.image} />
-            <h2 className='mr-8 text-sm ' >{item.title}</h2>
+         <div className=' grid grid-cols-1  mt-10 mb-10'>
+            <div className='bg-slate-200 w-80 h-40 rounded-2xl  shadow-lg shadow-gray-500/30   '>
+          <div key={item.id} className='bg-slate-100 rounded-2xl w-28 h-24 mt-4 ml-5 flex' >
+            <img className=' h-20 w-20 ' src={item.image} />
+            <h2 className=' text-sm ml-20 mt-10' >{item.title}</h2>
            
 
           </div>
--- src/components/containers/Team.jsx
+++ src/components/containers/Team.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 import {TeamData} from  "../data/TeamData";
-
+import NavBar from "../common/NavBar";
 import TeamCard from "../cards/TeamCards"
 
 
 function Team() {
   return (
-    <div>
-  
+    <div className="team flex flex-col justify-start items-center w-screen h-screen">
+  <NavBar/>
     <TeamCard data={TeamData}/>
   
     </div>
