e1e39739d9600a4986f60e1513bc4255c73e3b62
unknown <maryamihsan601@gmail.com>
2024-06-20 20:12:43 +0300

    make emergency desktop page

--- src/components/containers/EmergencyContainer.jsx
+++ src/components/containers/EmergencyContainer.jsx
@@ -3,14 +3,21 @@ import Drawer from '../common/Drawer'
 import NavBar from '../common/NavBar'
 import ambulance from '../../assets/images/ambulance.png'
 import EmergencyForm from '../InputForm/EmergencyForm'
+import '../../assets/css/MediaQuery.css'
 // import '../../assets/css/Style.css'
 export default function EmergencyContainer() {
   return (
     <div className="EmergencyContainer flex flex-col justify-start items-center w-screen h-screen">
-        <Drawer className='fill-white'/>
+      <div className="Drawer">
+      <Drawer className=' fill-white '/>
+      </div>
+        
         <NavBar/>
-        <img src={ambulance} alt='ambulance' className='mt-[35px] mb-4'/>
-        <EmergencyForm/>
+       <div className="EmergencyContent">
+       <img src={ambulance} alt='ambulance' className='lg:w-[40%] mt-[35px] mb-4'/>
+       <div className="line hidden lg:block lg:w-1 bg-[#CA5050] bg:h-[60vh] bg:mr-[50px]"></div>
+       <EmergencyForm/>
+       </div>
     </div>
   )
 }
