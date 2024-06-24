2effd418df8ca7fe607342cfb64e4a2fe7ba7837
unknown <maryamihsan601@gmail.com>
2024-06-24 16:34:04 +0300

    fix navbar in emergency page for mobile view

--- src/components/common/NavBar.jsx
+++ src/components/common/NavBar.jsx
@@ -7,13 +7,13 @@ function NavBar() {
     <div className='nav  flex bg-[#323868] w-[90%] justify-center items-center rounded-[40px] mt-[40px]'>
       <ul className='items inline-flex m-3 gap-2 text-[15px] w-[100%] items-center justify-center'>
         <Link to='/team'>
-        <li className='item bg-[#181D3D] rounded-[25px] p-3 text-white  w-[30%]'>الفرق الطبية</li>
+        <li className='item bg-[#181D3D] rounded-[25px] p-3 text-white  w-[100%]'>الفرق الطبية</li>
         </Link>
        <Link to='/services'>
-       <li className='item bg-[#181D3D] rounded-[25px] p-3 text-white  w-[30%] text-center'>خدماتنا</li>
+       <li className='item bg-[#181D3D] rounded-[25px] p-3 text-white  w-[100%] text-center'>خدماتنا</li>
        </Link>
         <Link to='/'>
-        <li id='active' className='item  bg-[#181D3D] rounded-[25px] p-3 text-white  w-[36%]'>الصفحة الرئيسية</li>
+        <li id='active' className='item  bg-[#181D3D] rounded-[25px] p-3 text-white  w-[100%]'>الصفحة الرئيسية</li>
         </Link>
       </ul>
       <div className="Login">
