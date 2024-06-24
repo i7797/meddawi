56a44f6bc83e574d092c626732d577ac30e7bb00
nura <nooraliabdalhossen@gmail.com>
2024-06-24 22:54:01 +0300

    change the contain  of team

--- src/components/cards/TeamCards.jsx
+++ src/components/cards/TeamCards.jsx
@@ -8,11 +8,16 @@ import React from "react";
         {data.map((item) => (
          <div className=' grid grid-cols-1  mt-10 mb-10'>
             <div className='bg-slate-200 w-80 h-40 rounded-2xl  shadow-lg shadow-gray-500/30   '>
-          <div key={item.id} className='bg-slate-100  w-28 h-24 mt-4 ml-5 flex rounded-xl' >
+          <div key={item.id} className=' w-28 h-24 mt-10 ml-5 flex rounded-xl' >
             <img className=' h-20 w-20 rounded-xl' src={item.image} alt=" "/>
-            <h2 className=' text-sm ml-20 mt-10' >{item.title}</h2>
+            <div  className='  mr-10 ml-20 '>
+            <h2 className=' team-text text-sm ml-0 mt-0 flex'  > الاسم: {item.title} </h2>
            
+            <h2 className='team-text text-sm ml-0 mt-0 flex' > العمر: {item.age} سنة</h2>
+            <h2 className='team-text text-sm ml-0 mt-0 flex' >  سنوات الخبرة : {item.years } سنة</h2>
 
+
+            </div>
           </div>
           </div>
           </div>
