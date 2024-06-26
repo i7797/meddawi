90e39f2bac2526a6671d01d67ef9b493e68401a5
unknown <maryamihsan601@gmail.com>
2024-06-26 21:58:13 +0300

    perpare team page

--- src/components/containers/Team.jsx
+++ src/components/containers/Team.jsx
@@ -2,14 +2,14 @@ import React from "react";
 import {TeamData} from  "../../data/TeamData";
 import NavBar from "../common/NavBar";
 import TeamCard from "../cards/TeamCards"
+import '../../assets/css/MediaQuery.css'
 
 
 function Team() {
   return (
-    <div className="team flex flex-col justify-start items-center w-screen h-screen">
+    <div className="team flex flex-col justify-start items-center w-screen h-screen lg:bg-transparent  lg:bg-opacity-0 lg:bg-none">
   <NavBar/>
     <TeamCard data={TeamData}/>
-  
     </div>
   );
 }
