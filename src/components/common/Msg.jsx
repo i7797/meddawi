a7d15394c9b41b9e16e4d85ed5bc7b89c7a420eb
nura <nooraliabdalhossen@gmail.com>
2024-06-30 22:29:30 +0300

    add img on msg

--- src/components/common/Msg.jsx
+++ src/components/common/Msg.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
+import done from '../../assets/images/yes.png'
 
 export default function Msg() {
   return (
     <div className='bg-[#181D3D]'>
+      <img className='w-80 h-80 mr-10 mt-0 mb-6' src={done}/>
       <h1 className='text-white text-[30px]'>
         لقد تم ارسال طلبك و سوف يتم الاتصال بكم خلال دقائق
       </h1>
