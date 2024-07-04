a7f3124fed11b466b5390ad3981ea90101532d26
nura <nooraliabdalhossen@gmail.com>
2024-07-04 11:19:12 +0300

    fix login css

--- src/Auth/Login.jsx
+++ src/Auth/Login.jsx
@@ -21,31 +21,31 @@ export const Login = () => {
     <div className=" flex w-screen h-screen items-center justify-center flex-col">
         {/* <h1 className="text-[60px] h-[30%] text-center flex items-center justify-center text-[#CA5050] font-bold"><span>اهلا و سهلا بك من جديد</span></h1> */}
     <div className="flex flex-col items-center h-[70%] w-full ">
-      <label className="label text-[25px]  self-end color-[#272C52] mr-5 font-extrabold ">
+      <label className="label text-[25px]  self-end color-[#272C52] mr-5 font-extrabold lg:mr-80">
         :اسم المستخدم{" "} 
      
       </label> <br/>
 
       <input
-          className="input w-[90%] h-[12%] rounded-2xl mb-3 border-4 border-solid border-[#272C52] bg-white "
+          className="input w-[90%] h-[12%] rounded-2xl mb-3 border-4 border-solid border-[#272C52] bg-white lg:w-[30%] "
           type="text"
           required
           value={user}
           onChange={(e) => setUser(e.target.value)  }
         />
 
-      <label className="label text-[25px]  self-end color-[#272C52] mr-5 font-extrabold">
+      <label className="label text-[25px]  self-end color-[#272C52] mr-5 font-extrabold lg:mr-80">
          :كلمة السر{" "} 
     
       </label> <br/> 
       <input 
-          className="input w-[90%] h-[12%] rounded-2xl mb-3 border-4 border-solid border-[#272C52] bg-white"
+          className="input w-[90%] h-[12%] rounded-2xl mb-3 border-4 border-solid border-[#272C52] bg-white lg:w-[30%]"
           type="password"
           required
           value={password}
           onChange={(e) => setPassword(e.target.value)}
         />
-      <button className="login w-[40%] h-[10%] text-white text-2xl" type="button" onClick={handleLogin}>
+      <button className="login w-[40%] h-[10%] text-white text-2xl lg:w-[20%]" type="button" onClick={handleLogin}>
         
         تسجيل الدخول
       </button>
