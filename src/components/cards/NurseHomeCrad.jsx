d4296267cea6be438063011a3aa063f517d153c7
nura <nooraliabdalhossen@gmail.com>
2024-07-10 15:55:49 +0300

    add card for nurse home page

---
+++ src/components/cards/NurseHomeCrad.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import "../../assets/css/MediaQuery.css";
+import "../../assets/css/Style.css";
+import { Link } from "react-router-dom";
+export default function NurseHomeCards() {
+  return (
+    <div className=" flex flex-col   items-center justify-center">
+      <Link to='/nurseteam'>
+        <div className=" flex justify-center items-center w-80 h-24 rounded-3xl mt-10 mb-10 bg-blue-900">
+          <h1 className="text-black  text-sm	">فرقنا</h1>
+        </div>
+      </Link>
+
+      <Link to ='nurseprofile'>
+      <div className=" flex justify-center items-center w-80 h-24 rounded-3xl mt-10 mb-10 bg-blue-900">
+        <h1 className="text-black text-sm	">بروفايل </h1>
+      </div>
+      </Link>
+      
+      <Link to='/nursepost'>
+        <div className=" flex justify-center items-center  w-80 h-24 rounded-3xl mt-10 mb-10 bg-blue-900">
+          <h1 className="text-black  	"> نشر بوست</h1>
+        </div>
+      </Link>
+    </div>
+  );
+}
