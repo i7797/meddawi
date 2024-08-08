543d93e3c67eebd6f4df1c808f93feec07bccda2
unknown <maryamihsan601@gmail.com>
2024-08-08 22:26:09 +0300

    update dashboard logic

--- src/components/Dashboard/Dashboard.jsx
+++ src/components/Dashboard/Dashboard.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
 import EmergencyData from './EmergencyData'
+import ServiceData from './ServiceData'
 import admin from '../../assets/images/user.png'
 import image from '../../assets/images/dashboardImage.svg'
 import alarm from '../../assets/images/alarm.png'
@@ -13,10 +14,10 @@ function Dashboard() {
   return (
  <div className='flex w-screen h-screen bg-black '>
     <div className='dashboardContent flex bg-red-300 w-full'>
-       <aside className='bg-white w-1/4 h-full items-center justify-evenly flex flex-col'>
+       <aside className='bg-white w-[15%] h-full items-center justify-evenly flex flex-col'>
        <h1 className='font-extrabold text-2xl'>MEDDAWI</h1>
-       <div className='w-[60%] h-1/4 bg-black rounded-full flex justify-center items-center'>
-       <img src={admin} alt='admin image' className='w-3/4 ml-[15px]' />
+       <div className='w-[30%] h-[9%] bg-black rounded-full flex justify-center items-center'>
+       <img src={admin} alt='admin image' className='w-3/4 ml-[9px]' />
        </div>
       
          <Link to='/home'>Patient Home Page</Link>
@@ -42,33 +43,24 @@ function Dashboard() {
        </div>
        <div className='flex  justify-evenly bg-red w-full h-3/4 '>
         <div className='textDev flex justify-center bg-white rounded-3xl w-1/2 h-full'>
-         <div className='helloSection w-full flex flex-col justify-center items-start'>
-         {showEmergencyData && (
-          
-          <div className='mt-4 flex flex-col justify-center items-start'>
-            
-             <h1 className='hidden text-[30px] font-extrabold'>Hello,<br/>Maryam & Noor</h1>
-             <h4 className='hidden'>What we will do today?</h4>
-             <div className='hidden w-1/2 h-full'>
-                <img src={image} alt='welcome image' className='h-full'/>   
-             </div>
-            <EmergencyData />
-          </div>
-           )}
-           {
-            !showEmergencyData&&(
+        <div className='helloSection w-full flex flex-col justify-center items-start'>
+            {(showEmergencyData || showDailyServiceData) ? (
+              <div className='mt-4 flex flex-col justify-center items-start'>
+                {showEmergencyData && <EmergencyData />}
+                {showDailyServiceData && <ServiceData />}
+              </div>
+            ) : (
               <div className='w-full flex flex-col justify-start items-center h-full'>
-              <div className='flex flex-col items-center w-full h-[25%]'>
-              <h1 className=' text-[30px] font-extrabold'>Hello,<br/>Maryam & Noor</h1>
-              <h4 className=''>What we will do today?</h4>
+                <div className='flex flex-col items-center w-full h-[25%]'>
+                  <h1 className='text-[30px] font-extrabold'>Hello,<br/>Maryam & Noor</h1>
+                  <h4 className=''>What we will do today?</h4>
+                </div>
+                <div className='w-full h-[75%] self-center'>
+                  <img src={image} alt='welcome image' className='h-full'/>   
+                </div>
               </div>
-              <div className='w-full h-[75%] self-center'>
-                <img src={image} alt='welcome image' className='h-full'/>   
-             </div>
-              </div>
-            )
-           }
-         </div>
+            )}
+       </div>
         </div>
         <div className='notification flex flex-col justify-start items-center  w-1/4 h-full rounded-3xl'>
          <div className='bg-blue-400 w-full h-[40%] flex flex-col justify-evenly items-center rounded-3xl'>
@@ -106,23 +98,6 @@ function Dashboard() {
       </div>
       
        </div>
-     
-         {/* <div className='EmergencyData&serviceData  flex flex-col items-center w-1/2 bg-yellow-200 gap-3'>
-          <div className='EmergecyData bg-red-800 h-1/2 w-[90%] rounded-xl mt-3'>
-            <h1>devone</h1>
-          </div>
-          <div className='EmergecyData bg-red-800 flex h-1/2 w-[90%] rounded-3xl mt-3'>
-          <h1>devTwo</h1>
-          </div>
-         </div>
-         <div className='bg-green-300 w-1/2 flex flex-col items-center gap-3'>
-          <div className=' bg-gray-300 h-1/2 w-[90%] rounded-3xl mt-3'>
-             <h1>this is dev one of second column</h1>
-          </div>
-          <div className='bg-gray-300 h-1/2 w-[90%] rounded-3xl mt-3'>
-             <h1>this is dev two of second column</h1>
-          </div>
-         </div> */}
        </div>
     </div>
  </div>
