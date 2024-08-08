e5e7e05a849f774f7a4b5ac9641ff52a84f9bc8d
unknown <maryamihsan601@gmail.com>
2024-08-08 21:17:57 +0300

    edit emergency form input

--- src/components/InputForm/EmergencyForm.jsx
+++ src/components/InputForm/EmergencyForm.jsx
@@ -14,8 +14,55 @@ export default function EmergencyForm() {
   const [caseOfEmergency, setEmergencyCase] = useState('');
   const [addressOfPatient, setAddress] = useState('');
 
+  const [ipAddress , setIpAddress] = useState('')
+  const [geoInfo , setGeoInfo] = useState ({}) 
+
+  const getvisitorIP = async()=>{
+    try {
+       const response = await fetch('https://api.ipify.org')
+       const data = await response.text()
+       setIpAddress(data )
+    } catch (error) {
+      console.error('failed to fetch ip:' , error )
+    }
+    console.log(ipAddress)
+    
+    
+      }
+    
+    
+    const fetchIpInfo = async ()=>{
+      try {
+        const response = await fetch(`http://ip-api.com/json/${ipAddress}`)
+        const data = await response.json()
+        setGeoInfo(data)
+      } catch (error) {
+        console.error('failed to get user location' , error)
+      }
+      
+    }
+
+    //-----------------------
+  const sendMessage = () => {
+  const url = `https://web.whatsapp.com/send?phone=${phoneNo}&text=${patientLocation}&app_absent=0`;
+  window.open(url, '_blank');
+};
+//-----------------------
+    
+    useEffect(()=>{
+      getvisitorIP()
+      fetchIpInfo()
   
+    },[])
+    console.log(geoInfo)
+    const lat = geoInfo.lat
+    const long = geoInfo.lon
+    const phoneNo = '07700443588'
+    const patientLocation =`https://www.google.com/maps/@${lat},${long}`
+
+
   const handleSubmit = async (e) => {
+    
     e.preventDefault();
     const phonePattern = /^\d{11}$/;
     if (!phonePattern.test(phone)) {
@@ -32,6 +79,7 @@ export default function EmergencyForm() {
         
       });
       console.log('Your data has been sent');
+      sendMessage(); 
     } catch (e) {
       console.error('Error submitting data', e);
     }
