978c7c9fc870981898487631ac09efebda3bab53
nura <nooraliabdalhossen@gmail.com>
2024-08-10 20:09:52 +0300

    add style to  a profile

--- src/Sign/Profile.jsx
+++ src/Sign/Profile.jsx
@@ -58,47 +58,72 @@ const Profile = () => {
   };
 
   return (
-    <>
-      {currentUser ? (
+
+<div className=" flex items-center justify-center min-h-screen bg-slate-100 ">
+    <div className="bg-white shadow-md max-w-sm w-full text-center rounded-lg p-8  ">
+        <h1 className="text-2xl font-bold mb-4"> Profile </h1>
+        {currentUser ? (
         <>
-          <input
+        <div className="mb-5"> 
+        <input
             type="email"
             value={email}
             placeholder="Email"
             onChange={(e) => setEmail(e.target.value)}
-            className=""
-          />
+            className=" px-4 py-2 border border-gray-400 w-full rounded-lg focus:outline-none focus:border-blue-950"
+            />
+        </div>
+        <div className="mb-5"> 
+
           <input
             type="name"
             value={name}
             placeholder="Name"
             onChange={(e) => setName(e.target.value)}
-            className=""
+            className=" px-4 py-2 border border-gray-400 w-full rounded-lg focus:outline-none focus:border-blue-950"
           />
+        </div>
+        <div className="mb-5"> 
 
           <input
             type="number"
             value={age}
             placeholder="Age"
             onChange={(e) => setAge(e.target.value)}
-            className=""
+            className=" px-4 py-2 border border-gray-400 w-full rounded-lg focus:outline-none focus:border-blue-950"
           />
+        </div>
+        <div className="mb-5"> 
 
           <input
             type="text"
             value={location}
             placeholder="Location"
             onChange={(e) => setLocation(e.target.value)}
-            className=""
+            className=" px-4 py-2 border border-gray-400 w-full rounded-lg focus:outline-none focus:border-blue-950"
           />
-          <button onClick={handelsave}>Edit to save</button>
+                  </div>
 
-          <button onClick={handelLogout}>Logout</button>
+          <button 
+          className="bg-blue-950 w-full text-white py-2 px-4 rounded-lg mb-3 hover:bg-blue-600  transition duration-200"
+
+          
+          
+          onClick={handelsave}>Edit to save</button>
+
+          <button
+          className="bg-blue-950 w-full text-white py-2 px-4 rounded-lg  hover:bg-blue-600  transition duration-200"
+          
+          onClick={handelLogout}>Logout</button>
         </>
       ) : (
         <p>"you are not logged"</p>
       )}
-    </>
+    </div>
+</div>
+
+     
+  
   );
 };
 
