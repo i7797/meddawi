9b9e9db819681cb72cf52f674020987d5bc92036
nura <nooraliabdalhossen@gmail.com>
2024-08-13 17:45:57 +0300

    fix nav

--- src/components/common/NavBar.jsx
+++ src/components/common/NavBar.jsx
@@ -22,41 +22,54 @@ function NavBar() {
         <div className="hidden  lg:justify-end   lg:inline-flex   w-full lg:h-[30%]">
           <div className=" nav lg:inline-flex justify-center items-center  w-[80%]">
             <nav>
-              <ul className="  inline-flex m-3 gap2 text-[15px]  items-center justify-center w-full">
+              <ul className="  inline-flex m-3 gap2 text-[15px]  space-x-5 items-center justify-center w-full">
+
+              <Link to="/">
+                <li className=" text-blue-950 rounded-[25px] p-3 hover:text-white w-[130px] lg:w-full text-center font-bold">
+                {t("Home")}</li>
+                </Link>
+
+
+                <Link to="/aboutUs">
+                  <li className="hidden lg:block font-bold"> {t("About")}</li>
+                </Link>
+
+
+                <Link to="/services">
+                  <li className="  rounded-[25px] p-3 text-white w-[130px] lg:w-full text-center font-bold">
+                  {t("Services")}                  </li>
+                </Link>
+
+                <Link to="/team1">
+                <li className="   rounded-[25px] p-3 text-blue-950 w-[130px] lg:w-full text-center  font-bold">
+                {t("Medical Teams")}</li>
+                </Link>
+
                 {currentUser ? (
                   <>
-                    <li className="   rounded-[25px] p-3 text-white w-[130px] lg:w-full text-center ">
+                
+                    <li className="   rounded-[25px] p-3 text-white w-[130px] lg:w-full text-center font-bold ">
                       <Link to="/profile">{t("Profile")}</Link>
                     </li>
 
                     <li
                       onClick={handleLogout}
-                      className="  rounded-[25px] p-3 text-white w-[130px] lg:w-full text-center "
+                      className="  rounded-[25px] p-3 text-white w-[130px] lg:w-full text-center font-bold "
                     >
    {t("Logout")}                    </li>
                   </>
                 ) : (
                   <Link to="/signup">
-                    <li className="   rounded-[25px] p-3 text-white w-[130px] lg:w-full text-center ">
+                    <li className="   rounded-[25px] p-3 text-white w-[130px] lg:w-full text-center  font-bold">
                     {t("Login")}                    </li>
                   </Link>
                 )}
-                <Link to="/team1">
-                  <li className=""> {t("Medical Teams")}</li>
-                </Link>
+               
 
-                <Link to="/services">
-                  <li className="  rounded-[25px] p-3 text-white w-[130px] lg:w-full text-center">
-                  {t("Services")}                  </li>
-                </Link>
-                <Link to="/aboutUs">
-                  <li className="hidden lg:block"> {t("About")}</li>
-                </Link>
+           
+        
 
-                <Link to="/">
-                <li className="  rounded-[25px] p-3 text-white w-[130px] lg:w-full text-center">
-                {t("Home")}</li>
-                </Link>
+                
               </ul>
             </nav>
           </div>
