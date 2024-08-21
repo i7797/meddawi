2cd9dafe6239330fd57f2a98f5e739027cf9b22a
nura <nooraliabdalhossen@gmail.com>
2024-08-21 15:52:26 +0300

    fix translate icon in drawer

--- src/components/common/Drawer.jsx
+++ src/components/common/Drawer.jsx
@@ -15,23 +15,25 @@ import HomeIcon from "@mui/icons-material/Home";
 import MedicalInformationIcon from "@mui/icons-material/MedicalInformation";
 import Diversity3Icon from "@mui/icons-material/Diversity3";
 import LoginIcon from "@mui/icons-material/Login";
-import PersonIcon from "@mui/icons-material/Person";
-import LogoutIcon from "@mui/icons-material/Logout";
+import GTranslateIcon from '@mui/icons-material/GTranslate';import LogoutIcon from "@mui/icons-material/Logout";
 import TaskAltIcon from "@mui/icons-material/TaskAlt";
-import InfoIcon from '@mui/icons-material/Info';
-import MenuOpenIcon from '@mui/icons-material/MenuOpen';
-import DashboardCustomizeIcon from '@mui/icons-material/DashboardCustomize';
-import {useTranslation} from "react-i18next"
+import InfoIcon from "@mui/icons-material/Info";
+import MenuOpenIcon from "@mui/icons-material/MenuOpen";
+import DashboardCustomizeIcon from "@mui/icons-material/DashboardCustomize";
+import { useTranslation } from "react-i18next";
 
 const Navbar = () => {
   const { currentUser, logout } = useAuth();
   const navigate = useNavigate();
-  const { i18n,t}=useTranslation();
-
+  const { i18n, t } = useTranslation();
 
   const handleLogout = () => {
     logout();
     navigate("/"); // Redirect to sign-in page after logout
+  };
+  const toggleArEn = () => {
+    const newlanguage = i18n.language == "en" ? "ar" : "en";
+    i18n.changeLanguage(newlanguage);
   };
 
   const [state, setState] = React.useState({
@@ -53,153 +55,140 @@ const Navbar = () => {
   };
 
   const list = (anchor) => (
-    <> 
-    <div className=" mt-0 bg-blue-950">
-         <MenuOpenIcon className="ml-3 mb-3 mt-3 w-28 text-white " />
-         </div>
-    <Box
-      sx={{ width: anchor === "top" || anchor === "bottom" ? "auto" : 250 }}
-      role="presentation"
-      onClick={toggleDrawer(anchor, false)}
-      onKeyDown={toggleDrawer(anchor, false)}
-    >
-      
-      <List>
-        
-         
-
-        
-
-        <ListItem disablePadding>
-          <ListItemButton>
-            <ListItemIcon>
-              <HomeIcon sx={{ color: "#181D3D" }} />
-            </ListItemIcon>
-            <Link to="/">
-              {" "}
-              <ListItemText
-                sx={{ color: "#181D3D" }}
-                primary={t("Home")}
-
-              />{" "}
-            </Link>
-          </ListItemButton>
-        </ListItem>
-
-
-        <ListItem disablePadding>
-          <ListItemButton>
-            <ListItemIcon>
-              <InfoIcon sx={{ color: "#181D3D" }} />
-            </ListItemIcon>
-            <Link to="/aboutUs">
-              {" "}
-              <ListItemText
-                sx={{ color: "#181D3D" }}
-                primary={t("About")}
-
-              />{" "}
-            </Link>
-          </ListItemButton>
-        </ListItem>
-        
-        
-
-        <ListItem disablePadding>
-          <ListItemButton>
-            <ListItemIcon>
-              <TaskAltIcon sx={{ color: "#181D3D" }} />
-            </ListItemIcon>
-            <Link to="/askservice">
-              {" "}
-              <ListItemText
-                sx={{ color: "#181D3D" }}
-                primary={t( "Ongoing Care")}
-              />{" "}
-            </Link>
-          </ListItemButton>
-        </ListItem>
-
-        <ListItem disablePadding>
-          <ListItemButton>
-            <ListItemIcon>
-              <MedicalInformationIcon sx={{ color: "#181D3D" }} />
-            </ListItemIcon>
-            <Link to="/services">
-              {" "}
-              <ListItemText sx={{ color: "#181D3D" }}                primary={t("Services")}
- />
-            </Link>
-          </ListItemButton>
-        </ListItem>
-        <ListItem disablePadding>
-          <ListItemButton>
-            <ListItemIcon>
-              <Diversity3Icon sx={{ color: "#181D3D" }} />
-            </ListItemIcon>
-            <Link to="/team1">
-              {" "}
-              <ListItemText sx={{ color: "#181D3D" }}  
-                             primary={t("Team")}
- />
-            </Link>
-          </ListItemButton>
-        </ListItem>
-      </List>
-      <Divider />
-      <List>
-        {currentUser ? (
-          <>
-      
-            <ListItem disablePadding onClick={handleLogout}>
-              <ListItemButton>
-                <ListItemIcon>
-                  <LogoutIcon sx={{ color: "#181D3D" }} />
-                </ListItemIcon>
-                <ListItemText sx={{ color: "#181D3D" }}                 
-                primary={t("Logout")}
- />
-              </ListItemButton>
-            </ListItem>
-          </>
-        ) : (
+    <>
+      <div className=" mt-0 bg-blue-950">
+        <MenuOpenIcon className="ml-3 mb-3 mt-3 w-28 text-white " />
+      </div>
+      <Box
+        sx={{ width: anchor === "top" || anchor === "bottom" ? "auto" : 250 }}
+        role="presentation"
+        onClick={toggleDrawer(anchor, false)}
+        onKeyDown={toggleDrawer(anchor, false)}
+      >
+        <List>
           <ListItem disablePadding>
             <ListItemButton>
               <ListItemIcon>
-                <LoginIcon sx={{ color: "#181D3D" }} />
+                <HomeIcon sx={{ color: "#181D3D" }} />
               </ListItemIcon>
-              <Link to="/signup">
-                <ListItemText sx={{ color: "#181D3D" }}                 primary={t("Login")}
- />
+              <Link to="/">
+                {" "}
+                <ListItemText
+                  sx={{ color: "#181D3D" }}
+                  primary={t("Home")}
+                />{" "}
               </Link>
             </ListItemButton>
           </ListItem>
-        )}
-<ListItem disablePadding>
-            <ListItemButton onClick={()=> i18n.changeLanguage("ar")}>
-             
-             
-                <ListItemText sx={{ color: "#181D3D" }} primary={t("Arabic")} />
-              
+
+          <ListItem disablePadding>
+            <ListItemButton>
+              <ListItemIcon>
+                <InfoIcon sx={{ color: "#181D3D" }} />
+              </ListItemIcon>
+              <Link to="/aboutUs">
+                {" "}
+                <ListItemText
+                  sx={{ color: "#181D3D" }}
+                  primary={t("About")}
+                />{" "}
+              </Link>
             </ListItemButton>
           </ListItem>
-<ListItem disablePadding>
-            <ListItemButton onClick={()=> i18n.changeLanguage("en")}>
-             
-             
-                <ListItemText sx={{ color: "#181D3D" }} primary={t("English")} />
-              
+
+          <ListItem disablePadding>
+            <ListItemButton>
+              <ListItemIcon>
+                <TaskAltIcon sx={{ color: "#181D3D" }} />
+              </ListItemIcon>
+              <Link to="/askservice">
+                {" "}
+                <ListItemText
+                  sx={{ color: "#181D3D" }}
+                  primary={t("Ongoing Care")}
+                />{" "}
+              </Link>
             </ListItemButton>
           </ListItem>
 
+          <ListItem disablePadding>
+            <ListItemButton>
+              <ListItemIcon>
+                <MedicalInformationIcon sx={{ color: "#181D3D" }} />
+              </ListItemIcon>
+              <Link to="/services">
+                {" "}
+                <ListItemText
+                  sx={{ color: "#181D3D" }}
+                  primary={t("Services")}
+                />
+              </Link>
+            </ListItemButton>
+          </ListItem>
+          <ListItem disablePadding>
+            <ListItemButton>
+              <ListItemIcon>
+                <Diversity3Icon sx={{ color: "#181D3D" }} />
+              </ListItemIcon>
+              <Link to="/team1">
+                {" "}
+                <ListItemText sx={{ color: "#181D3D" }} primary={t("Team")} />
+              </Link>
+            </ListItemButton>
+          </ListItem>
+        </List>
+        <Divider />
+        <List>
+          {currentUser ? (
+            <>
+              <ListItem disablePadding onClick={handleLogout}>
+                <ListItemButton>
+                  <ListItemIcon>
+                    <LogoutIcon sx={{ color: "#181D3D" }} />
+                  </ListItemIcon>
+                  <ListItemText
+                    sx={{ color: "#181D3D" }}
+                    primary={t("Logout")}
+                  />
+                </ListItemButton>
+              </ListItem>
+            </>
+          ) : (
+            <ListItem disablePadding>
+              <ListItemButton>
+                <ListItemIcon>
+                  <LoginIcon sx={{ color: "#181D3D" }} />
+                </ListItemIcon>
+                <Link to="/signup">
+                  <ListItemText
+                    sx={{ color: "#181D3D" }}
+                    primary={t("Login")}
+                  />
+                </Link>
+              </ListItemButton>
+            </ListItem>
+          )}
 
+<ListItem disablePadding>
+            <ListItemButton  onClick={toggleArEn}>
+              <ListItemIcon>
+                <GTranslateIcon sx={{ color: "#181D3D" }} />
+              </ListItemIcon>
+            
+                {" "}
+                <ListItemText
+                  sx={{ color: "#181D3D" }}
+                  primary=
+                  {i18n.language === "en" ? t("Arabic") : t("English")}
 
+                />{" "}
+            
+            </ListItemButton>
+          </ListItem>
 
-
-
-      </List>
-  
-    </Box>
+         
+        </List>
+      </Box>
     </>
   );
 
@@ -217,8 +206,9 @@ const Navbar = () => {
                     </Drawer>
                 </React.Fragment>
             ))} */}
-<div></div>
-      <MenuIcon className ="ml-3 mt-3 text-white"
+      <div></div>
+      <MenuIcon
+        className="ml-3 mt-3 text-white"
         onClick={toggleDrawer("left", true)}
       />
 
